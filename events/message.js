ce0a4f4c773f8b8b0764ce7a29fce2e0262984b3
DropNod <79993842+DropNod@users.noreply.github.com>
2022-06-10 21:02:46 +0100

    1.03

--- events/message.js
+++ events/message.js
@@ -19,50 +19,51 @@ module.exports = async (client, message) => {
         catch {return false}
     }
     if (message.channel.id === mpChannelID && message.author.id != botID){
-        if (!message.member.roles.cache.some(role => (staffRoles.includes(role.id)))) {
-            if (message.content.startsWith(prefix + 'mp')) {
-                const args = message.content.split(/\s+/);
-                if (args.length >= 2 && /^<@!?(\d{17,19})>$/g.test(args[1])) {
-                    if (message.author.id != args[1].slice(2, 20)) {
-                        isInTheGuild = await isInTheGuild(args[1]);
-                        if (isInTheGuild === true) {
-                            if (!message.mentions.users.first().bot) {
-                                if (message.mentions.members.first().roles.cache.some(role => (role.id === mpOpenRoleID))) {
-                                    sendError(`Inutile d'effectuer une demande, ce membre possède le rôle <@&${mpOpenRoleID}>`);
-                                }
-                                else if (message.mentions.members.first().roles.cache.some(role => (role.id === mpClosedRoleID))) {
-                                    sendError(`Vous ne pouvez pas effectuer une demande de MP à un membre possédant le rôle <@&${mpClosedRoleID}>`);
-                                }
-                                else {
-                                    let mention = args[1];
-                                    if (args.length > 2) {
-                                        reason = '>';
-                                        args.splice(0, 2);
-                                        args.forEach(arg => reason = `${reason} ${arg}`);         
-                                    }
-                                    else {reason = ''}
-                                    const askMPEmbed = new Discord.MessageEmbed()
-                                        .setColor('#2F3136')
-                                        .setAuthor({name:'📩・Demande de MP'})
-                                        .setDescription(`**${message.author.username}** souhaite te parler en MP\n${reason}`)
-                                        .setThumbnail(message.author.displayAvatarURL())
-                                        .setFooter({text:`ID: ${message.author.id}`});
-                                    const embedButtons = new Discord.MessageActionRow()
-                                        .addComponents(new Discord.MessageButton().setCustomId('accept').setLabel('Accepter').setStyle('SECONDARY'))
-                                        .addComponents(new Discord.MessageButton().setCustomId('deny').setLabel('Refuser').setStyle('SECONDARY'));
-                                    message.channel.send({embeds:[askMPEmbed], components:[embedButtons]}).then(msg => {msg.edit({content:mention})});
-                                }
+        if (message.content.startsWith(prefix + 'mp')) {
+            const args = message.content.split(/\s+/);
+            if (args.length >= 2 && /^<@!?(\d{17,19})>$/g.test(args[1])) {
+                if (message.author.id != args[1].slice(2, 20)) {
+                    isInTheGuild = await isInTheGuild(args[1]);
+                    if (isInTheGuild === true) {
+                        if (!message.mentions.users.first().bot) {
+                            if (message.mentions.members.first().roles.cache.some(role => (role.id === mpOpenRoleID))) {
+                                sendError(`Inutile d'effectuer une demande, ce membre possède le rôle <@&${mpOpenRoleID}>`);
                             }
-                            else{sendError('Vous ne pouvez pas effectuer une demande de MP à un bot')}
+                            else if (message.mentions.members.first().roles.cache.some(role => (role.id === mpClosedRoleID))) {
+                                sendError(`Vous ne pouvez pas effectuer une demande de MP à un membre possédant le rôle <@&${mpClosedRoleID}>`);
+                            }
+                            else {
+                                let mention = args[1];
+                                if (args.length > 2) {
+                                    reason = '>';
+                                    args.splice(0, 2);
+                                    args.forEach(arg => reason = `${reason} ${arg}`);         
+                                }
+                                else {reason = ''}
+                                const askMPEmbed = new Discord.MessageEmbed()
+                                    .setColor('#2F3136')
+                                    .setAuthor({name:'📩・Demande de MP'})
+                                    .setDescription(`**${message.author.username}** souhaite te parler en MP\n${reason}`)
+                                    .setThumbnail(message.author.displayAvatarURL())
+                                    .setFooter({text:`ID: ${message.author.id}`});
+                                const embedButtons = new Discord.MessageActionRow()
+                                    .addComponents(new Discord.MessageButton().setCustomId('accept').setLabel('Accepter').setStyle('SECONDARY'))
+                                    .addComponents(new Discord.MessageButton().setCustomId('deny').setLabel('Refuser').setStyle('SECONDARY'));
+                                message.channel.send({embeds:[askMPEmbed], components:[embedButtons]}).then(msg => {msg.edit({content:mention})});
+                            }
                         }
-                        else {sendError("Cet utilisateur n'est plus présent sur le serveur")}
+                        else{sendError('Vous ne pouvez pas effectuer une demande de MP à un bot')}
                     }
-                    else {sendError('Vous ne pouvez pas effectuer une demande de MP à vous-même')}
+                    else {sendError("Cet utilisateur n'est plus présent sur le serveur")}
                 }
-                else {sendError('Vous devez utiliser la commande `?mp @membre` pour effectuer une demande de MP')}
-            } 
+                else {sendError('Vous ne pouvez pas effectuer une demande de MP à vous-même')}
+            }
             else {sendError('Vous devez utiliser la commande `?mp @membre` pour effectuer une demande de MP')}
-            message.delete()
+            message.delete();
+        } 
+        else if (!message.member.roles.cache.some(role => (staffRoles.includes(role.id)))) {
+            sendError('Vous devez utiliser la commande `?mp @membre` pour effectuer une demande de MP');
+            message.delete();
         }
     }
 }
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
     console.log('Launched !');
     client.user.setStatus('available');
-    client.user.setActivity('v1.02');
+    client.user.setActivity('v1.03');
 }
