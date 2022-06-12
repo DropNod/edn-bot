f073b0fe1255a1fe6499ddde1744bf7e901f958f
DropNod <79993842+DropNod@users.noreply.github.com>
2022-06-12 11:27:10 +0100

    1.05

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ client.on('ready', () => require('./events/ready.js')(client));
 client.on('messageCreate', message => require('./events/message.js')(client, message));
 client.on("interactionCreate", interaction => require('./events/interaction.js')(client, interaction));
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.TOKEN);
